b0456235141a2ad461d20f6fb489442931f9b195
doly kakkar <dollykakkar910@gmail.com>
2024-10-08 20:08:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -6,21 +6,31 @@ import { MyDashboard } from "./Pages/Dashboard/MyDashboard";
 import { LogIn } from "./Pages/LogIn";
 import { SignUp } from "./Pages/SignUp";
 import { ResetPassword } from "./Pages/ResetPassword";
+import NavBar from "./Components/NavBar";
+import MyJobs from "./Pages/Dashboard/MyJobs";
+import Setting from "./Pages/Dashboard/Setting";
+import ContactUs from "./Pages/ContactUs";
 
 function App() {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/about" element={<AboutUs />} />
-        <Route path="/dashboard" element={<MyDashboard/>}/>
-        <Route path="/login" element={<LogIn/>}/>
-        <Route path="/signup" element={<SignUp/>}/>
-        <Route path="/resetpassword" element={<ResetPassword />} /> 
-      </Routes>
-      <Footer />
-
-    </>
+      <>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/dashboard" element={<MyDashboard />} />
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/resetpassword" element={<ResetPassword />} />
+          <Route path="/settings" element={<Setting/>}/>
+          <Route path="/jobs" element={<MyJobs/>}/>
+          <Route path="/contact" element={<ContactUs/>}/>
+        </Routes>
+        {/* <ContactUs/> */}
+        {/* <MyJobs/> */}
+        {/* <Setting/> */}
+        <Footer />
+      </>
   );
 }
 
